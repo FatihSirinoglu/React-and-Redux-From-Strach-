3a9b1d1c515f679dbda2e1614669dd24045ae304
sSsiriusSs <m.fatih.abd@gmail.com>
2023-04-07 00:03:09 +0300

    repeated until props and children

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-	return <h2>Inline component</h2>;
+function BookList() {
+	return (
+		<section className="bookList">
+			<Book /> 
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const Book = () => {
+	const url =
+		"https://m.media-amazon.com/images/I/51sVzc2P-5L._SX258_BO1,204,203,200_QL70_ML2_.jpg";
+	const title = "İnsanlığımı Yitirirken";
+	const author = "Osamu Dazai";
+	return (
+		<article className="book">
+			<img src={url} alt={title} />
+			<h2>{title}</h2>
+			<h3>{author}</h3>
+		</article>
+	);
+};
 
-root.render(<Greeting />);
+// const Person = () => <h1>Fatih</h1>;
+// const Message = () => {
+// 	return <h2>Hello</h2>;
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
